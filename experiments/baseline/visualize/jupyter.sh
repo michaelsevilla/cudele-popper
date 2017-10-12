83058defb27dfaeccf8bfbf1457e3b99a9011da4
Michael Sevilla <mikesevilla3@gmail.com>
2017-10-11 21:43:14 -0500

    radosbench results
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/baseline/visualize/jupyter.sh
+++ experiments/baseline/visualize/jupyter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -e -x
-docker run --rm -v `pwd`:/home/jovyan/work -p 81:8888 jupyter/scipy-notebook
+docker run --rm -v `dirname $PWD`:/home/jovyan/work -p 81:8888 jupyter/scipy-notebook:c411f52fcc93
