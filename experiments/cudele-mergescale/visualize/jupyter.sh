dbdab19dc8b3a068e4f4663b74404954dc7056a8
Michael Sevilla <mikesevilla3@gmail.com>
2017-10-20 14:26:56 -0700

    paper: add some slowdown merge scale graphs
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/cudele-mergescale/visualize/jupyter.sh
+++ experiments/cudele-mergescale/visualize/jupyter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -e -x
-docker run --rm -v `dirname $PWD`:/home/jovyan/work -p 81:8888 jupyter/scipy-notebook:c411f52fcc93
+docker run --rm -v  `dirname $PWD | xargs dirname`:/home/jovyan/work -p 81:8888 jupyter/scipy-notebook:c411f52fcc93
