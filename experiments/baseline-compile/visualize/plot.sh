ec810e07f02fd6ad25871b7242ed4529f93f492c
Michael Sevilla <mikesevilla3@gmail.com>
2017-12-11 14:44:37 -0800

    change name from progFS to cudele
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/baseline-compile/visualize/plot.sh
+++ experiments/baseline-compile/visualize/plot.sh
@@ -11,16 +11,27 @@ cd ../results
 #   "tmp/graphite/whisper/issdm-12/mds_server/req_create.wsp" \
 #   "tmp/graphite/whisper/issdm-12/mds_server/req_readdir.wsp" \
 #   "tmp/graphite/whisper/issdm-12/mds_server/req_lookup.wsp" \
-#   ; do
+#   "tmp/graphite/whisper/issdm-12/mds_server/req_unlink.wsp" \
+#   "tmp/graphite/whisper/issdm-12/mds_server/req_rmdir.wsp" \
+#    ; do
 for m in \
-    "tmp/graphite/whisper/issdm-12/mds_server/req_unlink.wsp" \
-    "tmp/graphite/whisper/issdm-12/mds_server/req_rmdir.wsp" \
+    "tmp/graphite/whisper/issdm-12/cputotals/user.wsp" \
+    "tmp/graphite/whisper/issdm-12/nettotals/kbout.wsp" \
+    "tmp/graphite/whisper/issdm-12/nettotals/kbin.wsp" \
     ; do
+  # dump trace
   docker run \
     -v `pwd`/tmp:/tmp \
     --entrypoint=whisper-dump.py \
     michaelsevilla/graphite \
     $m > `basename $m`.out
-  sed -i "s/:/,/g" `basename ${m}`.out
+
+  # clean trace to prepare for pandas
+  docker run \
+    -v `pwd`:/root \
+    --entrypoint=/bin/bash \
+    michaelsevilla/graphite \
+    -c "sed -i \"s/:/,/g\" /root/`basename ${m}`.out"
+
 done
 cd -
