a5cfe1c4d5247d918d9e95b8973c49459b50a03b
Michael Sevilla <mikesevilla3@gmail.com>
2017-12-12 08:38:18 -0800

    benchmark: popperize compile/interfere, rerun compile (#38, #27)
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/baseline-compile/visualize/plot.sh
+++ experiments/baseline-compile/visualize/plot.sh
@@ -14,9 +14,11 @@ for m in \
    "tmp/graphite/whisper/node-4/mds_server/req_unlink.wsp" \
    "tmp/graphite/whisper/node-4/mds_server/req_rmdir.wsp" \
    "tmp/graphite/whisper/node-4/cputotals/user.wsp" \
+   "tmp/graphite/whisper/node-4/cpuload/avg1.wsp" \
    "tmp/graphite/whisper/node-4/nettotals/kbout.wsp" \
    "tmp/graphite/whisper/node-4/nettotals/kbin.wsp" \
-   ; do
+  ; do
+
   # dump trace
   docker run \
     -v `pwd`/tmp:/tmp \
@@ -32,4 +34,28 @@ for m in \
     -c "sed -i \"s/:/,/g\" /root/`basename ${m}`.out"
 
 done
+
+count=0
+for m in \
+  "tmp/graphite/whisper/node-1/diskinfo/writekbs/sdc.wsp" \
+  "tmp/graphite/whisper/node-2/diskinfo/writekbs/sdc.wsp" \
+  "tmp/graphite/whisper/node-3/diskinfo/writekbs/sdc.wsp" \
+  ; do
+
+  count=$((count+1))
+  # dump trace
+  docker run \
+    -v `pwd`/tmp:/tmp \
+    --entrypoint=whisper-dump.py \
+    michaelsevilla/graphite \
+    $m > node$count-`basename $m`.out
+
+  # clean trace to prepare for pandas
+  docker run \
+    -v `pwd`:/root \
+    --entrypoint=/bin/bash \
+    michaelsevilla/graphite \
+    -c "sed -i \"s/:/,/g\" /root/node$count-`basename ${m}`.out"
+done
+
 cd -
